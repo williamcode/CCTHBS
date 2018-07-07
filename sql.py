7aad8e998774fe70063078e60eb695caa323e2a7
HBS <HBS@fsdept_william>
2018-07-07 11:45:33 +0800

    sql module

---
+++ sql.py
@@ -0,0 +1,72 @@
+# encoding=utf-8 
+import MySQLdb
+import pymssql
+from Demos.security.sspi.validate_password import password
+class MYSQL():
+    def __init__(self):
+        self.host='10.9.1.52'
+        self.port=3316
+        self.user='root'
+        self.passwd='htoa8000'
+        self.db='oa8000'
+
+
+    def __connection(self):
+        try:
+            self.cnn=MySQLdb.connect(host=self.host,port=self.port,user=self.user,passwd=self.passwd,db=self.db,charset='utf8',use_unicode=True)
+           
+        except:
+            pass
+    def Sqlexe(self,sql):
+        
+        self.__connection()
+        if self.cnn:
+            cur=self.cnn.cursor()
+            cur.execute(sql)
+            res=cur.fetchall()
+            cur.close()
+            self.cnn.close()
+            return res
+        else:
+            return None
+    def exe_noqueryset(self,sql):
+        self.__connection()
+        if self.cnn:
+            self.cnn.cursor().execute(sql)
+            self.cnn.commit()
+            self.cnn.close()
+
+
+
+class MSSQL():
+    def __init__(self):
+        self.host='10.9.1.52'
+        self.port=3316
+        self.user='root'
+        self.passwd='htoa8000'
+        self.db='oa8000'
+        
+    def __connection(self):
+        try:
+            self.cnn=pymssql.connect(host = self.host,user=self.user,password=self.passwd,database=self.db)
+        except:
+            pass
+
+    def Sqlexe(self,sql):
+        
+        self.__connection()
+        if self.cnn:
+            cur=self.cnn.cursor()
+            cur.execute(sql)
+            res=cur.fetchall()
+            cur.close()
+            self.cnn.close()
+            return res
+        else:
+            return None
+    def exe_noqueryset(self,sql):
+        self.__connection()
+        if self.cnn:
+            self.cnn.cursor().execute(sql)
+            self.cnn.commit()
+            self.cnn.close()
