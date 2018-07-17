1d5a927c10faa0050b7ae08a86c269be0e689e01
williamcode <xieshangyou.student@sina.com>
2018-07-17 20:30:19 +0800

    format account code

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main():
                 from 资金收取_主表 a inner join 资金收取_明细 b  on a.excelserverrcid=b.excelserverrcid where month(a.日期)=%d and year(a.日期)=%d
             """
     bank_rec_sql_re = msquery.Sqlexe(bank_rec_sql%(m,y))
-    
+
     if bank_rec_sql_re:
         sql.extend(bank_rec_sql_re)
         # type,日期，币种，手续费，银行金额，收款银行，账套，‘’，金额（字符型），客户,收款币种，合同号，b.excelserverrcid
@@ -71,20 +71,33 @@ def main():
 
     result=sorted(sql,key=itemgetter(1,12))
     #print result[0]
-   
+
     #formate the result 日期，摘要，科目，币种，记账币金额，外币金额，对方科目
     unit={1:'深圳市华讯方舟企业服务有限公司'.decode('utf-8'),
-          2:'华讯方舟企业服务有限公司'.decode('utf-8'),
-          3:'湖北典伦进出口贸易有限公司'.decode('utf-8')
+          2:'华讯方舟企业服务有限公司'.decode('utf-8')
+
           }
+    sql_km='''select 科目名称,科目代码 from 科目表
+            '''
+    sql_kh='''select ccusname,ccuscode from customer
+            '''
+    sql_gys='''select cvenname,cvencode from vendor
+                '''
+    dist_km=dist(msquery.Sqlexe(sql_km))
+    msquery.db='003_2018'
+    dist_kh_sz=dist(msquery.Sqlexe(sql_kh))
+    dist_gys_sz=dist(msquery.Sqlexe(sql_gys))
+    
+
+
     voucher_make=Voucher()
     for _re in result:
-            
+
         re=voucher_make.voucher_make(_re,unit[1])
         if re:
             for detail in re:
-                
-        
+
+
 
 
 if __name__=='__main__':
